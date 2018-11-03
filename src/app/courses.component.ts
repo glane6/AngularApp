c40a6def3e765c609de070b881f069022a6990dc
Gregory Lane <greg@Gregorys-MacBook-Pro.local>
2018-11-03 16:39:01 -0400

    Added email service, courses service and modified courses component

--- src/app/courses.component.ts
+++ src/app/courses.component.ts
@@ -1,10 +1,27 @@
-import {Component} from '@angular/core';
+import { Component } from '@angular/core';
+import { CoursesService } from './courses.service';
 
 @Component({
   selector: 'courses',
-  template: '<h2>Courses</h2>'
+  template: `
+  <img [src]="imageUrl" />
+  <table>
+      <tr>
+        <td [attr.colspan]="colSpan"></td>
+      </tr>
+    </table>
+  `
+
 })
 
 export class CoursesComponent {
+    title = 'List of courses';
+    courses;
+    imageUrl = '';
+    colSpan = 2;
 
+    constructor(service: CoursesService){
+      // let service = new CoursesService();
+      this.courses = service.getCourses();
+    }
 }
---
+++ src/app/courses.service.ts
@@ -0,0 +1,6 @@
+export class CoursesService{
+  getCourses(){
+    let courses = ['course1', 'course2', 'course3'];
+    return courses;
+  }
+}
